c7468e086cc8ef7c815a99d6a18d528e9f04e91e
Konstantin Solovyev <k1kollov@gmail.com>
2023-05-31 21:08:27 +0300

    audit

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -1,11 +1,45 @@
 import React from "react";
 import { Button } from "./button";
-import renderer from "react-test-renderer";
+import { render, screen, fireEvent } from '@testing-library/react';
 
-describe("Button component tests", ()=>{
-    it("Button with text test", ()=>{
+const renderer = require("react-test-renderer")
+
+
+const callback = ()=>{
+    alert("success")
+}
+
+describe("Button component test", ()=>{
+    it("with text", ()=>{
         const tree = renderer.create(<Button text="SomeText"/>).toJSON();
         expect(tree).toMatchSnapshot();
+    })
+
+    it("without text", ()=>{
+        const tree = renderer.create(<Button/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+
+    it ('onload', ()=>{
+        const tree = renderer.create(<Button isLoader={true}/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+
+    it('disabled', ()=>{
+        const tree = renderer.create(<Button disabled={true}/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+
+    it("event", ()=>{
+        window.alert = jest.fn();
+
+        render(<Button text="testButton" onClick={callback}/>)
+
+        const button = screen.getByText("testButton");
+
+        fireEvent.click(button)
+
+        expect(window.alert).toHaveBeenCalledWith("success")
     })
 })
 
